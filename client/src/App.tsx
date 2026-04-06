95edf80bd93a7775241c967a46d1bd138c36acc8
Codigoaprendiza02 <riyanshiverma123@gmail.com>
2026-04-06 10:07:22 +0530

    Build routes using react router, and navigations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,25 @@
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material"
+import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material"
 import { useMemo } from "react"
 import { themeSettings } from "./theme"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "./scenes/navbar"
 
 function App() {
   const theme = useMemo(()=> createTheme(themeSettings), [])
   return (
     <>
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<div>Dashboard page</div>} />
+            <Route path="/prediction" element={<div>Prediction Page</div>} />
+          </Routes>
+        </Box>
       </ThemeProvider>
+    </BrowserRouter>
     </>
   )
 }
