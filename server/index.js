d4f726f3aab65d297e60e2944c8982e50b224b45
Codigoaprendiza02 <riyanshiverma123@gmail.com>
2026-04-06 17:32:23 +0530

    Mongoose setup and configurations

--- server/index.js
+++ server/index.js
@@ -18,4 +18,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 console.log("Hello World");
+
 /* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGODB_URI).then(async() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`));
